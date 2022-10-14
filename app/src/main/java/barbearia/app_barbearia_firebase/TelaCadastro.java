c2a784f6761b209f01803edc0b171f9ac8dfb6ee
Andre Monteiro <andre_lpmonteiro1@hotmail.com>
2022-10-14 17:10:48 -0300

    App Barber

---
+++ app/src/main/java/barbearia/app_barbearia_firebase/TelaCadastro.java
@@ -0,0 +1,14 @@
+package barbearia.app_barbearia_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_main);
+    }
+}
---
+++ app/src/main/java/barbearia/app_barbearia_firebase/TelaLogin.java
@@ -0,0 +1,12 @@
+package barbearia.app_barbearia_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaLogin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+    }
+}
